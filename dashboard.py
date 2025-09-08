cacff3b03c704ba89a103eb3786161e8ee2e4168
itskleb <itskleb@gmail.com>
2025-09-08 15:57:12 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,9 +7,9 @@ st.title("CFB Spread Viewer")
 df = pd.read_csv("CFB_Spread_Database.csv")
 
 teams = df['homeTeam'].unique().tolist()
-
-week = st.selectbox("Week",[i for i in range(1,17)])
-
+with st.sidebar:
+  week = st.selectbox("Week",[i for i in range(1,17)])
+  team = st.selectbox("Team",df['homeTeam'].sort_values().unique().tolist())
 df = df[df['week'] == week]
 
 df
