2a3e1dc90daaa54b395c1729795b997e4437461f
itskleb <itskleb@gmail.com>
2025-09-08 15:53:07 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ df = pd.read_csv("CFB_Spread_Database.csv")
 
 teams = df['homeTeam'].unique().tolist()
 
-week = st.selectbox("Week",[i for i in range(1,17)],index=df['week'][0])
+week = st.selectbox("Week",[i for i in range(1,17)])
 
 df = df[df['week'] == week]
 
