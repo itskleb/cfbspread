ebf817bf9c12cd746dbd889d76fd2649f235c6e7
itskleb <itskleb@gmail.com>
2025-09-08 15:47:27 -0400

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.title("CFB Spread Viewer")
+
+df = pd.read_csv("CFB_Spread_Database.csv")
+
+teams = df['homeTeam'].unique().tolist()
+
+week = st.selectbox([i for i in range(1,17)],df['week'][-1])
+
+df = df[df['week'] == week]
+
+df
